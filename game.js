58c34f97c6d290d4f2ce93fad015fcbe67981a8e
JaviS <qnang5284@gmail.com>
2022-06-08 20:57:01 +0200

    a falta de revisión de documentación Kboom.js

--- game.js
+++ game.js
@@ -4,12 +4,45 @@ kaboom ( {
     fullscreen: true,
     scale: 1,
     debug: true,
+    background: [100, 170, 255, 1],
 });
+
+// Load all sprites from imgur
+loadRoot("https://i.imgur.com/");
+loadSprite("coin", "wbKxhcd.png");
+loadSprite("brick", "pogC9x5.png");
+
+
 
 // sets the "canvas" for the game to run into
 scene("game", () => {
+    layers(['bg', 'obj', 'ui'], 'obj');
 
+    // Create a map for this scene
+    const map = [
+        "                                                ",
+        "                                                ",
+        "                                                ",
+        "                                                ",
+        "                                                ",
+        "                                                ",
+        "           $                                    ",
+        "                                                ",
+        "                                                ",
+        "==========================  ===========  =======",
+    ];
+
+    // Define configuration for each symbol in the map.
+    const levelCfg = {
+        width: 20, // in pixels
+        height: 20,
+        "=": [sprite("brick", solid())],
+        '$': [sprite("coin")],
+    };
+
+    // set game level
+    const gameLevel = addLevel(map, levelCfg);
 });
 
 // Starts game loop
-start("game");
+go("game");
