a829ce2efdc9e6dad2ad06f42adb1d300a653aab
JaviS <qnang5284@gmail.com>
2022-06-08 21:53:11 +0200

    following new tutorial, discard of previous sprites

--- game.js
+++ game.js
@@ -1,61 +1,17 @@
 // initialize kaboom.js with stated settings
+import kaboom from "kaboom";
+
 kaboom ( {
-    global: true,
-    fullscreen: true,
-    scale: 1,
-    debug: true,
-    background: [100, 170, 255, 1],
+    background: [134, 135, 247],
+    width: 320,
+    height: 240,
+    scale: 2,
 });
 
-// Load all sprites from imgur
+// Let's load all the sprites
 loadRoot("assets/sprites/");
-loadSprite("coin", "wbKxhcd.png");
-loadSprite("brick", "pogC9x5.png");
+// load each sprite
 
 
 
-// sets the "canvas" for the game to run into
-scene("game", () => {
-    layers(['bg', 'obj', 'ui'], 'obj');
 
-    // Create a map for this scene
-    const map = [
-        "                                                ",
-        "                                                ",
-        "                                                ",
-        "                                                ",
-        "                                                ",
-        "                                                ",
-        "           $                                    ",
-        "                                                ",
-        "                                                ",
-        "==========================  ===========  =======",
-        "==========================  ===========  =======",
-    ];
-
-    // Define configuration for each symbol in the map.
-    const levelCfg = {
-        width: 20, // in pixels
-        height: 20,
-        "=": [
-            sprite("brick"), 
-            area(), 
-            solid(), 
-            origin("bot"), 
-            "brick"
-        ],
-        '$': [
-            sprite("coin"),
-            area(),
-            solid(),
-            origin("bot"),
-            "coin"
-        ],
-    };
-
-    // set game level
-    const gameLevel = addLevel(map, levelCfg);
-});
-
-// Starts game loop
-go("game");
